612243e90e7f1328db3270a9ca08adafaa648841
Michaelis’700701’git config --global user.name Michaelis’700701/usr/local/bin/python3 "/Users/michaeliacobelli/Visual Studio Code Programs/Simulations/sim_3_gravity-1/code/simulation.py"git config --global user.email ‘michael52012@hotmail.com’git config --global user.name Michaelis700701’ <michael52012@hotmail.com>
2024-09-20 09:36:37 -0400

    Added draw chart

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from pygame.math import Vector2 as vec
 pygame.init()
 
 # Set up the display
-DISPLAY_WIDTH, DISPLAY_HEIGHT = (1280 // 3), (800 // 2)
+DISPLAY_WIDTH, DISPLAY_HEIGHT = 500, 100
 display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 pygame.display.set_caption("Celsius Fahrenheit Converter")
 
@@ -25,11 +25,25 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 # Initialize more variables and states below
-thermometer_image = pygame.image.load("assets/thermometer.png")
+celsius = 0
+font = pygame.Font("assets/text/Arial.ttf", 20)
+
+celsius_text = font.render("CELSIUS", True, BLACK)
+fahrenheit_text = font.render("FAHRENHEIT", True, BLACK)
+celsius_value_text = font.render("0", True, BLACK)
+fahrenheit_value_text = font.render("0", True, BLACK)
+
+quadrant_rect_list = [
+    pygame.Rect((0, 0), (DISPLAY_WIDTH // 2, DISPLAY_HEIGHT // 2)),
+    pygame.Rect((DISPLAY_WIDTH // 2, 0), (DISPLAY_WIDTH // 2, DISPLAY_HEIGHT // 2)),
+    pygame.Rect((0, DISPLAY_HEIGHT // 2), (DISPLAY_WIDTH // 2, DISPLAY_HEIGHT // 2)),
+    pygame.Rect((DISPLAY_WIDTH // 2, DISPLAY_HEIGHT // 2), (DISPLAY_WIDTH // 2, DISPLAY_HEIGHT // 2)),
+]
 
 
 # Functions
 def get_temp() -> None:
+    global celsius
     # gets fahrenheit input from user
     fahrenheit = float(input('User input in Fahrenheit: '))
 
@@ -39,6 +53,26 @@ def get_temp() -> None:
     # prints a f-string so that I don't have to concatanate 
     # I learned this on my own outside the course
     print(f'This is your temperature in celsius: {celsius}')
+
+
+def draw_chart() -> None:
+    """
+    Draws the temperature chart.
+
+    Parameters:
+        None
+    """
+    for index, quadrant_rect in enumerate(quadrant_rect_list):
+        pygame.draw.rect(display, BLACK, quadrant_rect, 1)
+
+        if index == 0:
+            display.blit(celsius_text, (quadrant_rect.centerx - celsius_text.get_width() / 2, quadrant_rect.centery - celsius_text.get_height() / 2))
+        elif index == 1:
+            display.blit(celsius_value_text, (quadrant_rect.centerx - celsius_value_text.get_width() / 2, quadrant_rect.centery - celsius_value_text.get_height() / 2))
+        elif index == 2:
+            display.blit(fahrenheit_text, (quadrant_rect.centerx - fahrenheit_text.get_width() / 2, quadrant_rect.centery - fahrenheit_text.get_height() / 2))
+        else:
+            display.blit(fahrenheit_value_text, (quadrant_rect.centerx - fahrenheit_value_text.get_width() / 2, quadrant_rect.centery - fahrenheit_value_text.get_height() / 2))
 
 
 def event_loop() -> None:
@@ -62,7 +96,8 @@ def draw() -> None:
         None
     """
     display.fill(WHITE)
-    display.blit(thermometer_image, (0,0))
+
+    draw_chart()
 
 
 # Define main loop
@@ -73,6 +108,7 @@ while True:
     event_loop()
 
     # Update game state
+
 
     # Draw graphics and output
     draw()
